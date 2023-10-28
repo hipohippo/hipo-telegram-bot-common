0c0cf56dbc82579006869af6a52c45f70d161b2a
hipo <hipo@github>
2023-10-28 17:52:49 -0400

    minor change

--- src/hipo_telegram_bot_common/selenium_driver/launch.py
+++ src/hipo_telegram_bot_common/selenium_driver/launch.py
@@ -8,20 +8,40 @@ from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.firefox.options import Options as firefox_options
 
 
+def tchrome():
+    options = chrome_options()
+    #   options.add_extension(os.path.join(config["chromedriver_path"], config["chrome_bypass_paywall_extension"]))
+    options.add_experimental_option("debuggerAddress", f"127.0.0.1:19287")
+    # options.add_argument("--user-data-dir=/home/hipo/bots/selenium-driver/chrome-profile")
+    # options.add_argument("--headless=new")
+    # options.add_argument("--no-sandbox")
+    # options.binary_location="/opt/google/chrome/google-chrome"
+    epath = "/home/hipo/botrun/selenium-driver/chromedriver-linux64/chromedriver"
+    # driver = webdriver.Chrome(executable_path=epath, options=options)
+    driver = webdriver.Chrome(executable_path=epath)
+    driver.set_page_load_timeout(5)
+    driver.get("http://www.baidu.com")
+    driver.get(
+        "http://www.sina.com.cn",
+    )
+    time.sleep(5)
+
+
 def get_chrome_driver(config: dict, load_extension: False, given_port: Optional[int]) -> WebDriver:
     options = chrome_options()
     if load_extension:
-        options.add_extension(os.path.join(config["chromedriver_path"], config["chrome_bypass_paywall_extension"]))
+        options.add_argument(f'load-extension={config["chrome_bypass_paywall_extension"]}')
     if given_port:
-        options.add_experimental_option("debuggerAddress", f"127.0.0.1:{given_port}")
+        pass
+        # options.add_experimental_option("debuggerAddress", f"127.0.0.1:{given_port}")
+        # options.add_argument("user-data-dir=/home/hipo/bots/selenium-driver/chrome-profile")
+        # options.add_argument("--headless=")
+        # options.add_argument("--no-sandbox")
+    print(os.path.join(config["chromedriver_path"], config["chrome_driver"]))
     driver = webdriver.Chrome(
         executable_path=os.path.join(config["chromedriver_path"], config["chrome_driver"]), options=options
     )
-    time.sleep(5)
-    while len(driver.window_handles) < int(config.get("max_web_driver_window", 1)):
-        driver.execute_script("""window.open("http://www.google.com","_blank");""")
-        time.sleep(2)
-    driver.switch_to.window(driver.window_handles[0])
+    # driver.switch_to.window(driver.window_handles[int(config["designated_window_handle"])])
     return driver
 
 
